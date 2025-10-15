b5099a57b66f1d0cbc37a7920d14c3c8e55dac96
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-10-15 11:31:06 -0400

    create player with form in HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage(){
-    return <h1>This is the Home Page</h1>
+    const [username, setUsername] = useState("");
+    // const nav = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:3000/api/players", {
+                username,
+            });
+
+            const player = res.data;
+            // console.log(player)
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    
+    return (
+        <div className="home-page">
+            <h1>BOOMTATO</h1>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    placeholder="Enter your username..."
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <button type="submit">Create Player</button>
+            </form>
+        </div>
+    )
 }
