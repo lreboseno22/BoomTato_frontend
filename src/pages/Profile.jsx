e65bb43668264ad63dc200c7660bb054f169de97
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-10-15 11:59:29 -0400

    delete profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -22,6 +22,15 @@ export default function ProfilePage(){
         getPlayer();
     }, [id]);
 
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`http://localhost:3000/api/players/${id}`)
+            nav("/"); // Go back Home
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     if(!player) return <p>Loading...</p>
 
     return (
@@ -29,6 +38,7 @@ export default function ProfilePage(){
             <h1>Player Profile</h1>
             <h2>{player.username}</h2>
             <p>Score: {player.score}</p>
+            <button onClick={handleDelete}>Delete Profile</button>
         </div>
     )
 }
