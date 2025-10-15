cc2a114f5d5de3de0ba12f9e0dddb10ea5cef128
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-10-15 10:52:09 -0400

    inital set up with react router

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from './pages/Home';
+import LeaderboardPage from './pages/Leaderboard';
+import ProfilePage from './pages/Profile';
+
+function App() {
+
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/leaderboard">Leaderboard</Link>
+        <Link to="/profile">Profile</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/leaderboard' element={<LeaderboardPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
