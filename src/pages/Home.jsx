8856ba313b70025a26fb6aa27924c5f0885af3b8
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-10-22 00:42:59 -0400

    added styling to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import styles from "../styles/Home.module.css";
 
 export default function HomePage(){
     const nav = useNavigate();
     
     return (
-        <div className="home-page">
-            <h1>BOOMTATO</h1>
-            <div className="btns-container">
-                <button onClick={() => nav("/register")}>Register</button>
-                <button onClick={() => nav("/login")}>Login</button>
+        <div className={styles.homePage}>
+            <h1 className={styles.title}>BOOMTATO</h1>
+            <div className={styles.btnsContainer}>
+                <button onClick={() => nav("/register")} className={styles.btn}>Register</button>
+                <button onClick={() => nav("/login")} className={styles.btn}>Login</button>
             </div>
         </div>
     )
