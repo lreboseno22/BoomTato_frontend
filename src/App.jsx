4c92f4881d70ce07a5ee95cce0a903020ee29321
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-11-08 16:36:50 -0500

    remove unused pages and navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/Home';
-import LeaderboardPage from './pages/Leaderboard';
 import ProfilePage from './pages/Profile';
-import RegisterPage from './pages/Register';
-import LoginPage from './pages/Login';
 import LobbyPage from './pages/Lobby';
-import Navbar from './components/Navbar';
 import GamePage from './pages/Game';
 import KaboomCanvas from './pages/KaboomCanvas';
 
@@ -14,13 +10,9 @@ function App() {
 
   return (
     <div>
-      {/* <Navbar /> */}
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/login' element={<LoginPage />} />
         <Route path='/lobby' element={<LobbyPage />} />
-        <Route path='/leaderboard' element={<LeaderboardPage />} />
         <Route path='/profile/:id' element={<ProfilePage />} />
         <Route path='/game/:id' element={<GamePage />} />
         <Route path='/play/:id' element={<KaboomCanvas />} />
