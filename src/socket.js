d6a0938839b5ab0ece27e2b576435fb5913bf6cb
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-11-09 22:05:53 -0500

    reverse order for polling so websocket connection can be established

--- src/socket.js
+++ src/socket.js
@@ -2,8 +2,7 @@ import { io } from "socket.io-client";
 
 // socket instance
 const socket = io(import.meta.env.VITE_API_URL, {
-    transports: ["websocket", "polling"],
-    path: "/socket.io",
+    transports: ["polling", "websocket"],
 });
 
 export default socket;
