7ec657b46ea8711995983136714dd96c9ce8f12f
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-11-09 21:55:41 -0500

    attempt to setup websockets correctly with railway

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 // socket instance
 const socket = io(import.meta.env.VITE_API_URL, {
-    transports: ["websocket"],
+    transports: ["websocket", "polling"],
     path: "/socket.io",
 });
 
