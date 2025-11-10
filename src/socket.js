ddacb599be3a79e889448aa466939c69e132a43b
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-11-09 19:48:55 -0500

    deploying to vercel add VITE API URL

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
 // socket instance
-const socket = io("http://localhost:3000");
+const socket = io(import.meta.env.VITE_API_URL || "http://localhost:3000", {
+    transports: ["websocket"],
+});
 
 export default socket;
